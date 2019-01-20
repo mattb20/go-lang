d27aae23f9ff0176bb643da797e5d9910672075e
mattb20 <matt_burgoyne@live.co.uk>
2019-01-20 19:17:00 +0000

    add function to calc sum of two ints

--- packages.go
+++ packages.go
@@ -6,7 +6,12 @@ import (
   "math"
 )
 
+func add(x, y int) int {
+  return x + y
+}
+
 func main() {
   fmt.Println("My favourite number is", rand.Intn(10))
   fmt.Println(math.Pi)
+  fmt.Println(add(42,13))
 }
